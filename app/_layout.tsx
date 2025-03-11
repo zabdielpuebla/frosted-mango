5eea21c9e2bb68a7947274e062707d4d008a95af
wilson1146 <84942482+wilson1146@users.noreply.github.com>
2025-03-11 17:05:15 -0400

    Remove Header
    removed header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  
+  return <Stack 
+    screenOptions={{
+    
+    headerShown: false,
+    
+    
+    }}>
+  </Stack>;
 }
