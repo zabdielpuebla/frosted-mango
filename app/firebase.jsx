6996bdce3b72e584487840653ef6cb3f13d602ab
wilson1146 <84942482+wilson1146@users.noreply.github.com>
2025-04-04 20:23:16 -0400

    Integrated Firebase

---
+++ app/firebase.jsx
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxWPBO2n53DJrfwHndRhejx-IGvSwQC3I",
+  authDomain: "budgetmobile-22e2d.firebaseapp.com",
+  projectId: "budgetmobile-22e2d",
+  storageBucket: "budgetmobile-22e2d.firebasestorage.app",
+  messagingSenderId: "77496733627",
+  appId: "1:77496733627:web:3bc2d6a14249785950be9e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
